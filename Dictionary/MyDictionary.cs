da7f201b7f12d9afc89015993aedb601dda4c303
İREM ÜSTÜN <16030411060@ybu.edu.tr>
2021-03-28 00:54:09 +0300

    Proje dosyası ekle.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TKeys, TValues>
+    {
+        TKeys[] Keys;
+        TValues[] Values;
+
+        public MyDictionary()
+        {
+            Keys = new TKeys[0];
+            Values = new TValues[0];
+
+        }
+        
+        public void Add(TKeys Key, TValues Value)
+        {
+            TKeys[] tempKeys = Keys;
+            Keys = new TKeys[Keys.Length + 1];
+
+            TValues[] tempValues = Values;
+            Values = new TValues[Values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                Keys[i] = tempKeys[i];
+            }
+            Keys[Keys.Length - 1] = Key;
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                Values[i] = tempValues[i];
+            }
+            Values[Keys.Length - 1] = Value;
+        }
+        public TValues GetValuesByKeys(TKeys Key)
+        {
+            int index = Array.IndexOf(Keys, Key);
+            return Values[index];
+
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> List = new MyDictionary<string, string>();
+
+            List.Add("1", "Masa");
+            List.Add("2", "Kalem");
+            List.Add("3", "Silgi");
+
+            Console.WriteLine(List.GetValuesByKeys("1"));
+            Console.WriteLine(List.GetValuesByKeys("2"));
+            Console.WriteLine(List.GetValuesByKeys("3"));
+
+        }
+    }
+}
